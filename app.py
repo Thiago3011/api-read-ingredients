603fa807ded093b36f947a6c1f8d5faf7c8efa43
Thiago3011 <thiago.silva1001@outlook.com>
2025-07-22 13:17:22 -0300

    finalizando lógica básica

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-ingredients_list = [
+ingredients_user_list = [
     "farinha de trigo",
     "açúcar",
     "cacau em pó",
@@ -14,27 +14,32 @@ ingredients_list = [
     "sal"
 ]
 
-allergy_items = [
+allergy_user_items = [
     "farinha de trigo",
     "cacau em pó"
 ]
 
+@app.route('/')
 def index():
     return "teste"
 
 @app.route('/ingredients', methods=["POST"])
 def get_ingredients():
-    ingredients_list_checked = check_allergy_items(ingredients_list, allergy_items)
-    print(ingredients_list_checked)
+    allergy_list_checked, ingredients_list_checked = check_allergy_items(ingredients_user_list, allergy_user_items)
 
-    return jsonify({"received": ingredients_list_checked}), 200
+    return jsonify({"allergy_items": allergy_list_checked}, {"ingredients_list_checked": ingredients_list_checked}), 200
 
-def check_allergy_items(ingredients_list, allergy_items):
-    for item in ingredients_list:
-        if item in allergy_items:
-            print(f"Voce é alergico ao item {item}")
+def check_allergy_items(ingredients_user_list, allergy_user_items):
+    allergy_items = []
+    not_allergy_items = []
+
+    for item in ingredients_user_list:
+        if item in allergy_user_items:
+            allergy_items.append(item)
         else:
-            print("Não há itens alergicos")
+            not_allergy_items.append(item)
+
+    return allergy_items, not_allergy_items
 
 if __name__ == "__main__":
     app.run(debug=True)
