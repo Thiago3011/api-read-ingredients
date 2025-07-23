4dfe7ae7f7f8b168b8e32e2e8b269a43805e9faa
Thiago3011 <thiago.silva1001@outlook.com>
2025-07-23 00:53:11 -0300

    configurando a integração do html com o python garantindo o funcionamento da checagem

--- app.py
+++ app.py
@@ -1,38 +1,37 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
-
-ingredients_user_list = [
-    "açúcar",
-    "cacau em pó",
-    "fermento em pó",
-    "ovos",
-    "leite",
-    "óleo vegetal",
-    "essência de baunilha",
-    "sal"
-]
 
 allergy_user_items = [
     "farinha de trigo",
     "cacau em pó"
 ]
 
-@app.route('/')
+@app.route('/components')
 def index():
     return render_template('index.html')
 
-@app.route('/ingredients', methods=["POST"])
-def get_ingredients():
-    allergy_list_checked = check_allergy_items(ingredients_user_list, allergy_user_items)
+@app.route('/components', methods=["POST"])
+def get_components():
 
-    return jsonify({"allergy_items": allergy_list_checked}), 200
+    try:
+        components_user_list = request.form.getlist('components[]')
+        allergy_list_checked = check_allergy_items(components_user_list, allergy_user_items)
 
-def check_allergy_items(ingredients_user_list, allergy_user_items):
+        if allergy_list_checked:
+            return jsonify({"allergy_items": allergy_list_checked}), 200
+        else:    
+            return jsonify({"Mensagem": "Você não é alérgico a nada!"}), 200
+        
+    except Exception as error:
+
+        return jsonify({"error": str(error)}), 500
+
+def check_allergy_items(components_user_list, allergy_user_items):
     allergy_items = []
     not_allergy_items = []
 
-    for item in ingredients_user_list:
+    for item in components_user_list:
         if item in allergy_user_items:
             allergy_items.append(item)
         else:
--- static/script.js
+++ static/script.js
@@ -1,9 +1,10 @@
-const allergyForm = document.getElementById("ingredients-form")
+const allergyForm = document.getElementById("components-form")
 
 function increaseAllergy() {
     let newInput = document.createElement('input')
-    newInput.name = "ingredients[]"
+    newInput.name = "components[]"
     newInput.type = "text";
+    newInput.placeholder = "Insira um novo componente..."
     
     const buttonSubmit = allergyForm.querySelector('button[type="submit"]')
     allergyForm.insertBefore(newInput, buttonSubmit)
