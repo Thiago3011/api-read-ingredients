9d213eb40ee3a0b2b8015eee65c2c824db7aa638
Thiago3011 <thiago.silva1001@outlook.com>
2025-07-23 14:00:22 -0300

    Configurando o envio de imagens e seus processamentos

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, render_template, request
+from PIL import Image
+import pytesseract
 
 app = Flask(__name__)
 
@@ -16,6 +18,9 @@ def get_components():
 
     try:
         components_user_list = request.form.getlist('components[]')
+        image_file = request.files.get('image')
+        image_text = process_image(image_file)
+        print(image_text)
         allergy_list_checked = check_allergy_items(components_user_list, allergy_user_items)
 
         if allergy_list_checked:
@@ -38,6 +43,11 @@ def check_allergy_items(components_user_list, allergy_user_items):
             not_allergy_items.append(item)
 
     return allergy_items
+
+def process_image(image_file):
+    image = Image.open(image_file)
+    text = pytesseract.image_to_string(image)
+    return text
 
 if __name__ == "__main__":
     app.run(debug=True)
