2f41110ec149299655219fbe7a7db65261371b29
Thiago3011 <thiago.silva1001@outlook.com>
2025-07-24 14:30:56 -0300

    trocando o modo para utilização ao inves de desenvolvedor

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def get_components():
 
         return jsonify({"error": str(error)}), 500
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
