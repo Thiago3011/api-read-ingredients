cbbf578a6ecf01389c74902c759d969e2e757678
Thiago3011 <thiago.silva1001@outlook.com>
2025-07-29 19:07:00 -0300

    desenvolvendo a página do retorno caso não haja alergias + inclusão de uma lib no html para fogos

--- app.py
+++ app.py
@@ -35,13 +35,13 @@ def get_components():
         if allergy_list_checked:
             return render_template('result.html', allergy_list=allergy_list_checked)
         else:    
-            return jsonify({"Sucesso": "Você não é alérgico a nada!"}), 200
+            return render_template('result.html'), 200
         
     except Exception as error:
         return jsonify({"error": str(error)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
 # if __name__ == "__main__":
-#     app.run(debug=True)
+#     app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True)
