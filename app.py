1112b9eac204f3d2935b1b7ba4d94d7bf7004004
Thiago3011 <thiago.silva1001@outlook.com>
2025-07-29 20:02:13 -0300

    habilitando para rodar no servidor

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def get_components():
     else:
         return render_template('index.html')
         
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
